dd5bfd475a1d0a3eed96444b9c6fe5c7bebbf23b
making <makingx@gmail.com>
2017-02-18 00:27:04 +0900

    Add login form

---
+++ src/main/java/com/example/AuthController.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+	@GetMapping("/")
+	String index() {
+		return "index";
+	}
+
+	@GetMapping("login")
+	String login() {
+		return "login";
+	}
+}
--- src/main/java/com/example/TweeterAuthApplication.java
+++ src/main/java/com/example/TweeterAuthApplication.java
@@ -2,6 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
@@ -10,5 +13,16 @@ public class TweeterAuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TweeterAuthApplication.class, args);
+	}
+
+	@Configuration
+	static class LoginConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.formLogin().loginPage("/login").permitAll().and().requestMatchers()
+					.antMatchers("/", "/login", "/oauth/authorize",
+							"/oauth/confirm_access")
+					.and().authorizeRequests().anyRequest().authenticated();
+		}
 	}
 }
