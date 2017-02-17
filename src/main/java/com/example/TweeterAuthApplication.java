58dd1ac97d3b92bbc127d45ae474c2305b83fa96
making <makingx@gmail.com>
2017-02-18 01:24:55 +0900

    Add /userinfo

--- src/main/java/com/example/TweeterAuthApplication.java
+++ src/main/java/com/example/TweeterAuthApplication.java
@@ -3,9 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @SpringBootApplication
 @EnableAuthorizationServer
@@ -23,6 +27,18 @@ public class TweeterAuthApplication {
 					.antMatchers("/", "/login", "/oauth/authorize",
 							"/oauth/confirm_access")
 					.and().authorizeRequests().anyRequest().authenticated();
+		}
+	}
+
+	@Configuration
+	@EnableResourceServer
+	static class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+					.authorizeRequests().antMatchers(HttpMethod.GET, "/userinfo")
+					.access("#oauth2.hasScope('openid')");
 		}
 	}
 }
---
+++ src/main/java/com/example/UserinfoController.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserinfoController {
+
+	@GetMapping("userinfo")
+	Object userinfo(Authentication authentication) {
+		Map<String, Object> userinfo = new LinkedHashMap<>();
+		userinfo.put("name", authentication.getName());
+		userinfo.put("authorities", authentication.getAuthorities().stream()
+				.map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
+		userinfo.put("firstName", "John");
+		userinfo.put("lastName", "Doe");
+		userinfo.put("email", "jdoe@example.com");
+		return userinfo;
+	}
+}
