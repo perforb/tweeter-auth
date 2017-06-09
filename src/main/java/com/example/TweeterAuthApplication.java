b52ca5ca03f0d29ef27b382d1b71b3bcd0a61127
making <makingx@gmail.com>
2017-06-09 18:42:20 +0900

    Support LDAP Authentication

--- src/main/java/com/example/TweeterAuthApplication.java
+++ src/main/java/com/example/TweeterAuthApplication.java
@@ -1,12 +1,18 @@
 package com.example;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -27,6 +33,26 @@ public class TweeterAuthApplication {
 					.antMatchers("/", "/login", "/oauth/authorize",
 							"/oauth/confirm_access")
 					.and().authorizeRequests().anyRequest().authenticated();
+		}
+
+		/**
+		 * http://docs.spring.io/spring-security/site/docs/4.2.3.RELEASE/reference/html/jc.html#ldap-authentication
+		 */
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.ldapAuthentication() //
+					.userDnPatterns("uid={0},ou=people") //
+					.groupSearchBase("ou=groups") //
+					.contextSource(contextSource()) //
+					.passwordCompare() //
+					.passwordEncoder(new LdapShaPasswordEncoder())
+					.passwordAttribute("userPassword");
+		}
+
+		@Bean
+		DefaultSpringSecurityContextSource contextSource() {
+			return new DefaultSpringSecurityContextSource(
+					Arrays.asList("ldap://localhost:8389/"), "dc=springframework,dc=org");
 		}
 	}
 
