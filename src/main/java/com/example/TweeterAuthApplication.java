bdcc6f03a9360779edb40636d6aeccb02eb99e38
perforb <dev.perfumed.garden@gmail.com>
2017-07-13 12:24:56 +0900

    Customized sample application

--- src/main/java/com/example/TweeterAuthApplication.java
+++ src/main/java/com/example/TweeterAuthApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+/**
+ * @see https://github.com/Pivotal-Japan/spring-security-oauth-workshop/blob/master/authorization-server.md
+ */
 @SpringBootApplication
 @EnableAuthorizationServer
-public class TweeterAuthApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TweeterAuthApplication.class, args);
-	}
+public class TweeterAuthApplication
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(TweeterAuthApplication.class, args);
+  }
 }
